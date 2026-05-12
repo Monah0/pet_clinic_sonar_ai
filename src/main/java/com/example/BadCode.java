6fe92f6954d2b4d23082f78dfcadee24fb145bcd
Ruslan Romanchuk <ruslan_romanchuk@epam.com>
2026-05-12 12:53:32 +0300

    fix code 2

--- src/main/java/com/example/BadCode.java
+++ src/main/java/com/example/BadCode.java
@@ -1,10 +1,29 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BadCode {
 
     public void test() {
+
         String s = null;
-        s.length();
+        s.length(); // NullPointer risk
+
+        if (true == true) {
+            System.out.println("debug");
+        }
+
+        List list = new ArrayList();
+
+        int unusedVariable = 42;
+
+        try {
+            int x = 1 / 0;
+        }
+        catch (Exception e) {
+        }
+
     }
 
 }
